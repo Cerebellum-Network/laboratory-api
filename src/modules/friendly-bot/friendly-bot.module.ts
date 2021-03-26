36e92c8badbeeda44e9b59fcafa81175516ae4b5
karthikeyan-cere <73122026+karthikeyan-cere@users.noreply.github.com>
2021-03-26 14:53:20 +0530

    Features/total issuance (#15)
    * adding total issuance api
    
    * updating rate limiter for friendbot

--- src/modules/friendly-bot/friendly-bot.module.ts
+++ src/modules/friendly-bot/friendly-bot.module.ts
@@ -1,21 +1,16 @@
-import {RateLimiterInterceptor, RateLimiterModule} from 'nestjs-rate-limiter';
+import {RateLimiterModule} from 'nestjs-rate-limiter';
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '../config/config.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {PayoutEntity} from './entities/payout.entity';
 import {FriendlyBotController} from './friendly-bot.controller';
 import {FriendlyBotService} from './friendly-bot.service';
-import {APP_INTERCEPTOR} from '@nestjs/core';
 
 @Module({
   imports: [ConfigModule, TypeOrmModule.forFeature([PayoutEntity]), RateLimiterModule],
   controllers: [FriendlyBotController],
   providers: [
     FriendlyBotService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: RateLimiterInterceptor
-    }
   ],
   exports: [],
 })
