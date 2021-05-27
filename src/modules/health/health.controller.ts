863ff963cf24a9129993adfc2f828774d2796406
Karthikeyan <karthikeyan@cere.io>
2021-05-27 15:49:46 +0530

    added multi network setup for health check

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, HttpStatus, Inject, Res} from '@nestjs/common';
+import {BadRequestException, Controller, Get, HttpStatus, Inject, Param, Res} from '@nestjs/common';
 import {
   ApiInternalServerErrorResponse,
   ApiGatewayTimeoutResponse,
@@ -23,60 +23,74 @@ export class HealthController {
   ) {}
 
   @ApiResponse({status: 200, description: 'System health fetched successfully.'})
-  @Get('health-check')
-  public healthCheck(): Promise<any> {
-    return this.healthService.healthCheck();
+  @Get('health-check/:network')
+  public healthCheck(@Param('network') network: string): Promise<any> {
+    if (this.healthService.network.has(network)) {
+      return this.healthService.healthCheck(network);
+    }
+    throw new BadRequestException(`Invalid network type.`);
   }
 
   @ApiResponse({status: 200, description: 'Block status info fetched successfully.'})
-  @Get('block-status')
-  public blockStatus(): Promise<BlockStatusDto> {
-    return this.healthService.blockStatus();
+  @Get('block-status/:network')
+  public blockStatus(@Param('network') network: string): Promise<BlockStatusDto> {
+    if (this.healthService.network.has(network)) {
+      return this.healthService.blockStatus(network);
+    }
+    throw new BadRequestException(`Invalid network type.`);
   }
 
-  @Get('finalization')
+  @Get('finalization/:network')
   @ApiResponse({status: 204, description: 'Finalization is healthy.'})
   @ApiNotFoundResponse({description: 'Finalization is unhealthy.'})
-  public async finalization(@Res() res: Response): Promise<any> {
-    const diff = await this.healthService.finalization();
-    if (diff) {
-      res.status(HttpStatus.NOT_FOUND).send();
-    }
-    res.status(HttpStatus.NO_CONTENT).send();
-  }
-
-  @Get('block-production')
-  @ApiResponse({status: 204, description: 'Block production is healthy.'})
-  @ApiNotFoundResponse({description: 'Block production is unhealthy.'})
-  public async blockProduction(@Res() res: Response): Promise<any> {
-    const diff = await this.healthService.blockProduction();
-    if (diff) {
+  public async finalization(@Param('network') network: string, @Res() res: Response): Promise<any> {
+    if (!this.healthService.network.has(network)) {
+      throw new BadRequestException(`Invalid network type.`);
+    } else {
+      const diff = await this.healthService.finalization(network);
+      if (diff) {
+        res.status(HttpStatus.NOT_FOUND).send();
+      }
       res.status(HttpStatus.NO_CONTENT).send();
     }
-    res.status(HttpStatus.NOT_FOUND).send();
   }
 
-  @Get('node-dropped')
+  @Get('block-production/:network')
+  @ApiResponse({status: 204, description: 'Block production is healthy.'})
+  @ApiNotFoundResponse({description: 'Block production is unhealthy.'})
+  public async blockProduction(@Param('network') network: string, @Res() res: Response): Promise<any> {
+    if (this.healthService.network.has(network)) {
+      const diff = await this.healthService.blockProduction(network);
+      if (diff) {
+        res.status(HttpStatus.NO_CONTENT).send();
+      }
+      res.status(HttpStatus.NOT_FOUND).send();
+    } else {
+      throw new BadRequestException(`Invalid network type.`);
+    }
+  }
+
+  @Get('node-dropped/:network')
   @ApiResponse({status: 204, description: 'Validator node is healthy.'})
   @ApiNotFoundResponse({description: 'Validator node is unhealthy.'})
-  public async nodeDropped(@Res() res: Response): Promise<any> {
-    const result = await this.healthService.nodeDropped();
-    if (result) {
-      res.status(HttpStatus.NO_CONTENT).send();
+  public async nodeDropped(@Param('network') network: string, @Res() res: Response): Promise<any> {
+    if (this.healthService.network.has(network)) {
+      const result = await this.healthService.nodeDropped(network);
+      if (result) {
+        res.status(HttpStatus.NO_CONTENT).send();
+      }
+      res.status(HttpStatus.NOT_FOUND).send();
+    } else {
+      throw new BadRequestException(`Invalid network type.`);
     }
-    res.status(HttpStatus.NOT_FOUND).send();
   }
 
-  @Get('node-dropped-status')
+  @Get('node-dropped-status/:network')
   @ApiNotFoundResponse({description: 'Validator node is unhealthy.'})
-  public nodeDroppedStatus(): Promise<any> {
-    return this.healthService.nodeDroppedStatus();
-  }
-
-  @Get('validator')
-  @ApiResponse({status: 204, description: 'Block production is healthy.'})
-  @ApiNotFoundResponse({description: 'Block production is unhealthy.'})
-  public validator(): Promise<any> {
-    return this.healthService.validatorSlashed();
+  public nodeDroppedStatus(@Param('network') network: string): Promise<any> {
+    if (this.healthService.network.has(network)) {
+      return this.healthService.nodeDroppedStatus(network);
+    }
+    throw new BadRequestException(`Invalid network type.`);
   }
 }
