07ce3638b638f521f03274d847454afea7dce526
Andrei Navoichyk <andrei@cere.io>
2021-06-22 12:19:50 +0300

    updated migration SQL

--- src/migrations/1624351161224-cleanUpDevnetScannedData.ts
+++ src/migrations/1624351161224-cleanUpDevnetScannedData.ts
@@ -2,11 +2,9 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class cleanUpDevnetScannedData1624351161224 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE FROM transactions WHERE "networkType" = 'TESTNET_DEV'`);
-        await queryRunner.query(`DELETE FROM transactions WHERE "networkType" = 'DEVNET'`);
+        await queryRunner.query(`DELETE FROM transactions WHERE "networkType"='TESTNET_DEV' OR "networkType"='DEVNET'`);
 
-        await queryRunner.query(`DELETE FROM blocks WHERE "networkType" = 'TESTNET_DEV'`);
-        await queryRunner.query(`DELETE FROM blocks WHERE "networkType" = 'DEVNET'`);
+        await queryRunner.query(`DELETE FROM blocks WHERE "networkType"='TESTNET_DEV' OR "networkType"='DEVNET'`);
     }
 
     public down(queryRunner: QueryRunner): Promise<void> {
