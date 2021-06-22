55e3918ff70604d1f0012c3297acf9111e302809
Andrei Navoichyk <andrei@cere.io>
2021-06-22 12:00:53 +0300

    clean up Devnet scanned data migration is added

---
+++ src/migrations/1624351161224-cleanUpDevnetScannedData.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class cleanUpDevnetScannedData1624351161224 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM transactions WHERE "networkType" = 'TESTNET_DEV'`);
+        await queryRunner.query(`DELETE FROM transactions WHERE "networkType" = 'DEVNET'`);
+
+        await queryRunner.query(`DELETE FROM blocks WHERE "networkType" = 'TESTNET_DEV'`);
+        await queryRunner.query(`DELETE FROM blocks WHERE "networkType" = 'DEVNET'`);
+    }
+
+    public down(queryRunner: QueryRunner): Promise<void> {
+        return undefined;
+    }
+}
