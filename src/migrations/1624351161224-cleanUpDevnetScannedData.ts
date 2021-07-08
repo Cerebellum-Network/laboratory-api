ee345592dc4249105c66692cb69056b5136d8d2f
Andrei Navoichyk <andrei@cere.io>
2021-07-08 17:24:51 +0300

    commented test migration

--- src/migrations/1624351161224-cleanUpDevnetScannedData.ts
+++ src/migrations/1624351161224-cleanUpDevnetScannedData.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// import {MigrationInterface, QueryRunner} from "typeorm";
 
 // export class cleanUpDevnetScannedData1624351161224 implements MigrationInterface {
 //     public async up(queryRunner: QueryRunner): Promise<void> {
