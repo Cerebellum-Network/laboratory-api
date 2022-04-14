c33cb2cc37c65b6f537f7534e8ab5fdda057a8d5
Andrei Navoichyk <andrei@cere.io>
2022-04-15 00:46:24 +0400

    updated docker files

--- libs/database/src/ormconfig.ts
+++ libs/database/src/ormconfig.ts
@@ -13,11 +13,11 @@ const ormConfig: TypeOrmModuleOptions = {
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_DATABASE'),
-  synchronize: !!configService.get('DB_SYNCHRONIZE'),
+  synchronize: configService.get('DB_SYNCHRONIZE') === 'true',
   entities: configService.get('DB_ENTITIES').split(' '),
   logging: configService.get('DB_LOGGING') && configService.get('DB_LOGGING') === 'true',
   maxQueryExecutionTime: parseInt(configService.get('DB_MAX_QUERY_EXECUTION_TIME') || '100', 10),
-  keepConnectionAlive: !!configService.get('DB_CONNECTION_ALIVE'),
+  keepConnectionAlive: configService.get('DB_CONNECTION_ALIVE') === 'true',
   migrations: [`${migrationsDir}/*${migrationsFiles}`],
   cli: {
     migrationsDir,
